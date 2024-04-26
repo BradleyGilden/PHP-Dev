ab9060f025a4a558441f537d91e76d003e39eff7
BradleyGilden <bradleygilden@gmail.com>
2024-04-26 23:06:17 +0200

    methods

---
+++ procedural_basics/methods.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * variable paramters similar to rest operator: function(...$val) {}
+ * type hints (only throw error when strict types is declared): function(int $a, int $b): int {}
+ * pass by reference function (&$a) {}
+ * functions are hoisted just like js
+ */
