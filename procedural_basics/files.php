a17d1cb4dc562e5c18baeb96a62fcb0ad380c0a7
BradleyGilden <bradleygilden@gmail.com>
2024-04-29 16:37:37 +0200

    read and write files

--- procedural_basics/files.php
+++ procedural_basics/files.php
@@ -9,8 +9,27 @@ function fsize($filename): void {
     else echo 'Not Found<br />';
 }
 
+function readPrint($filename): void {
+    // Open the file for reading
+    $file = fopen($filename, "r");
+
+    // Loop through each line until the end of the file
+    while (!feof($file)) {
+        // Read the current line
+        $line = fgets($file);
+        
+        // Process the line (e.g., echo it)
+        echo $line;
+    }
+    // Close the file
+    fclose($file);
+}
+
 pprint(scandir((__DIR__)));
 var_dump(is_dir(scandir(__DIR__)[0]));
 echo '<br />';
 fsize('index.php');
 fsize('undefined.php');
+
+file_put_contents('file.txt', 'hello world');
+readPrint('file.txt');
