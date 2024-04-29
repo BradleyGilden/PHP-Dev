4de8203c8881beac63b196bfd8e0524865b61143
BradleyGilden <bradleygilden@gmail.com>
2024-04-29 21:42:57 +0200

    public methods

--- OOP/classesObjects.php
+++ OOP/classesObjects.php
@@ -1,13 +1,20 @@
 <?php 
 
 class Transaction {
-    private string $id = '234920KDjfsl';
-    public float $amount;
-    public string $description;
+    private static int $cid = 0;
+    private int $id;
+    private float $amount;
+    private string $description;
 
     public function __construct($amount=0.0, $description='no info')
     {
+        Transaction::$cid++;
+        $this->id = Transaction::$cid;
         $this->amount = $amount;
         $this->description = $description;
+    }
+
+    public function getId(): int {
+        return $this->id;
     }
 }
--- OOP/index.php
+++ OOP/index.php
@@ -5,5 +5,9 @@ require_once('../procedural_basics/utils.php');
 require_once('./classesObjects.php');
 
 $transaction = new Transaction(15.0, 'for uber');
+$transaction2 = new Transaction(65.43, 'Hairstylest');
 
 pprint($transaction);
+pprint($transaction2);
+
+pprint(['ids' => [$transaction->getId(), $transaction2->getId()]]);
