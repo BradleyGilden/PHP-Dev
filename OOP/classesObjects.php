92a8b6ea8d9057972151b6989a6ed24d915ab460
BradleyGilden <bradleygilden@gmail.com>
2024-04-29 21:45:10 +0200

    using self

--- OOP/classesObjects.php
+++ OOP/classesObjects.php
@@ -8,13 +8,18 @@ class Transaction {
 
     public function __construct($amount=0.0, $description='no info')
     {
-        Transaction::$cid++;
-        $this->id = Transaction::$cid;
+        // self referes to the class itself (equivalent to Transaction:$cid++)
+        self::$cid++;
+        $this->id = self::$cid;
         $this->amount = $amount;
         $this->description = $description;
     }
 
     public function getId(): int {
         return $this->id;
+    }
+
+    public static function objCount(): int {
+        return self::$cid;
     }
 }
