e386f77840d3b03d03bae22bfb55b7838580385c
BradleyGilden <bradleygilden@gmail.com>
2024-05-04 19:22:41 +0200

    mysql integration

--- src/mysql/index.php
+++ src/mysql/index.php
@@ -1,7 +1,32 @@
 <?php
 require '../../vendor/autoload.php';
+require_once('../basics/utils.php');
 // load environment
 use Dotenv\Dotenv;
+
+function printRow(array $row) {
+    $size = 9;
+    foreach($row as $item) {
+        $size = max($size, strlen($item));
+    }
+    pprint(
+    str_pad('ID', $size) . ' | ' .
+    str_pad('FNAME', $size) . ' | ' .
+    str_pad('SNAME', $size) . ' | ' . 
+    str_pad('PAY(HRLY)', $size) . ' | ' .
+    str_pad('DATE', $size) . ' | ' .
+    str_pad('BOSS_ID', $size)
+    );
+    pprint(
+        str_pad($row[0], $size) . ' | ' .
+        str_pad($row[1], $size) . ' | ' .
+        str_pad($row[2], $size) . ' | ' . 
+        str_pad($row[3], $size) . ' | ' .
+        str_pad($row[4], $size) . ' | ' .
+        str_pad($row[5], $size)
+    );
+    pprint(str_repeat('-', 100));
+}
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -9,17 +34,23 @@ $dotenv->load();
 $host = $_ENV['DB_HOST'];
 $user = $_ENV['DB_USER'];
 $pwd = $_ENV['DB_PWD'];
-$mysqli = new mysqli($host, $user, $pwd);
 
-if($mysqli→connect_errno ) {
-    printf("Connect failed: %s<br />", $mysqli→connect_error);
+$conn = new mysqli($host, $user, $pwd, "myDB");
+
+if($conn→connect_errno) {
+    printf("Connect failed: %s<br />", $conn→connect_error);
     exit();
 }
-printf('Connected successfully.<br />');
-$mysqli→close();
 
-// if (class_exists('mysqli')) {
-//     echo "mysqli is installed";
-// } else {
-//     echo "Enable Mysqli support in your PHP installation "; 
-// }
+$query = 'SELECT * FROM employees';
+
+$res = $conn->query($query);
+
+while ($row = $res->fetch_row()) {
+    printRow($row);
+}
+
+$res->close();
+
+printf('Connected successfully.<br />');
+$conn→close();
