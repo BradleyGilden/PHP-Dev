e75db75a6bf38e35d5428be363c4991cba10dedb
BradleyGilden <bradleygilden@gmail.com>
2024-05-07 20:13:35 +0200

    simple while loop

--- wordpress/university-theme/index.php
+++ wordpress/university-theme/index.php
@@ -1,4 +1,11 @@
 <h1><?= bloginfo('name'); ?></h1>
-<h2><?= bloginfo('description') ?></h2>
+<h3><?= bloginfo('description') ?></h3>
 
-This is a custom theme
+<hr />
+
+<?php while(have_posts()): ?>
+    <?= the_post(); ?>
+    <h2><?= the_title() ?></h2>
+    <p><?= the_content() ?></p>
+    <hr />
+<?php endwhile; ?>
