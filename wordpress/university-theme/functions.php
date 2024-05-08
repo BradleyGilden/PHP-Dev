e95276bd43cf271631ab2ea7f021dc3fe75024de
BradleyGilden <bradleygilden@gmail.com>
2024-05-08 20:36:12 +0200

    page depencies

--- wordpress/university-theme/functions.php
+++ wordpress/university-theme/functions.php
@@ -1,9 +1,19 @@
 <?php
 
     function uni_resources() {
-        wp_enqueue_style('main_styles', get_stylesheet_uri());
+        wp_enqueue_script('main_script', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+        wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+        wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+        wp_enqueue_style('main_styles', get_theme_file_uri('/build/style-index.css'));
+        wp_enqueue_style('extra_styles', get_theme_file_uri('/build/index.css'));
+    }
+
+    function uni_features() {
+        add_theme_support('title-tag');
     }
 
     // wp_enqueue_scripts for loading static files
     add_action('wp_enqueue_scripts', 'uni_resources');
+
+    add_action('after_setup_theme', 'uni_features');
 ?>
--- wordpress/university-theme/page.php
+++ wordpress/university-theme/page.php
@@ -1,7 +1,47 @@
 <!-- This is a template for pages -->
+
 <body>
-    <p style="position: absolute; right: 10px;"><a href=<?= admin_url() ?> > DASHBOARD </a></p>
-    <h1>This Screen is Powered by page.php</h1>
-    <h2><?= the_title() ?></h2>
-    <p><?= the_content() ?></p>
+  <?php
+  get_header();
+  while (have_posts()) {
+    the_post(); ?>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?= the_title() ?></h1>
+        <div class="page-banner__intro">
+          <p>REPLACE ME LATER</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+      <?php $parent_id = wp_get_post_parent_id(get_the_ID());
+      if ($parent_id) { ?>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($parent_id) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent_id); ?></a> <span class="metabox__main"><?php echo the_title(); ?></span>
+        </p>
+      </div>
+      <?php } ?>
+
+      <?php $absolute_parent = $parent_id ? $parent_id : get_the_ID() ?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($absolute_parent) ?>"><?php echo get_the_title($absolute_parent); ?></a></h2>
+        <ul class="min-list">
+          <?php wp_list_pages( array(
+            'title_li' => NULL,
+            'child_of' => $absolute_parent
+          ));
+          ?>
+        </ul>
+      </div>
+
+      <div class="generic-content">
+        <p><?= the_content() ?></p>
+      </div>
+    </div>
+  <?php }
+  get_footer();
+  ?>
 </body>
