56f3d4509d9c4f535a8c68c0a77157a7baca06e2
BradleyGilden <bradleygilden@gmail.com>
2024-05-12 00:02:46 +0200

    custom queries

--- wordpress/university-theme/archive.php
+++ wordpress/university-theme/archive.php
@@ -10,7 +10,7 @@
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title"><?php echo $headline ?></h1>
     <div class="page-banner__intro">
-      <p>Keep up with out latest news</p>
+      <p><?php the_archive_description() ?></p>
     </div>
   </div>
 </div>
