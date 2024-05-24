ac499ab4f4b944d8fb58daa0eb3a4204004daa39
BradleyGilden <bradleygilden@gmail.com>
2024-05-24 20:23:59 +0200

    started search bar

--- wordpress/university-theme/functions.php
+++ wordpress/university-theme/functions.php
@@ -4,6 +4,7 @@
     function uni_resources() {
         // loads static resources
         wp_enqueue_script('main_script', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+        wp_enqueue_script('search_script', get_theme_file_uri('/js/Search.js'), array('jquery'), '1.0', true);
         wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
         wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
         wp_enqueue_style('main_styles', get_theme_file_uri('/build/style-index.css'));
