7cf17092afb5dcdfef32627f72ad1e1c8a03e10d
BradleyGilden <bradleygilden@gmail.com>
2024-06-07 22:08:21 +0200

    type delay

--- wordpress/university-theme/src/modules/Search.js
+++ wordpress/university-theme/src/modules/Search.js
@@ -1,14 +1,38 @@
 const $ = window.jQuery;
 
 const search = () => {
-  $(".js-search-trigger").on("click", function () {
+  let isOpen = false;
+  let typingTimer;
+  // display search field
+  $(".js-search-trigger").on("click", () => {
     $(".search-overlay").toggleClass("search-overlay--active");
     $("body").toggleClass("body-no-scroll");
   });
-  $(".search-overlay__close").on("click", function () {
+
+  // close search field
+  $(".search-overlay__close").on("click", () => {
     $(".search-overlay").toggleClass("search-overlay--active");
     $("body").toggleClass("body-no-scroll");
   });
+
+  // escape to close
+  $(document).on("keyup", (event) => {
+    if (event.keyCode === 27 && isOpen) {
+      $(".search-overlay").removeClass("search-overlay--active");
+      $("body").removeClass("body-no-scroll");
+      isOpen = false;
+    } else if(event.keyCode === 83 && !isOpen) {
+      $(".search-overlay").addClass("search-overlay--active");
+      $("body").addClass("body-no-scroll");
+      isOpen = true;
+    }
+  })
+
+  // typing
+  $("#search-term").on("input", (event) => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(() => { console.log("Word Typed") }, 1000);
+  })
 };
 
 export default search;
